ed1b4ee1d6de1b6ce09cd7108ce9a3479a9708ba
dilorom4444 <abdujalilova2003@gmail.com>
2025-11-18 12:06:30 +0500

    Create  hw-1.py

---
+++ lesson-1/homework /hw-1.py
@@ -0,0 +1,30 @@
+
+a = float(input("Enter side of square: "))
+perimeter = 4 * a
+area = a * a
+print("Perimeter:", perimeter)
+print("Area:", area)
+
+d = float(input("Enter diameter of circle: "))
+circle_length = 3.14159 * d
+print("Circle length:", circle_length)
+
+a1 = float(input("Enter first number: "))
+b1 = float(input("Enter second number: "))
+mean = (a1 + b1) / 2
+print("Mean:", mean)
+a2 = float(input("Enter number a: "))
+b2 = float(input("Enter number b: "))
+
+sum_ab = a2 + b2
+product_ab = a2 * b2
+square_a = a2 * a2
+square_b = b2 * b2
+
+print("Sum:", sum_ab)
+print("Product:", product_ab)
+print("Square of a:", square_a)
+print("Square of b:", square_b)
+
+
+
